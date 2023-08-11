81b95ddbc451e655147956edafb5b919e0aeb767
XSY <624848971@qq.com>
2023-08-11 21:33:55 +0800

    配置了新建问卷和填写问卷的Activity

--- app/src/main/java/com/example/test/Fragment1.java
+++ app/src/main/java/com/example/test/Fragment1.java
@@ -1,18 +1,43 @@
 package com.example.test;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Fragment1 extends Fragment {
+
+    private Button newButton;
+    private Button writeButton;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_1, container, false);
+
+        newButton = view.findViewById(R.id.newButton);
+        writeButton = view.findViewById(R.id.writeButton);
+
+        newButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), NewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        writeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), WriteActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
